05545823e52c4a949a8ebf11d30001b0ad2115be
CodyGuo <guoxiao219@126.com>
2015-05-19 22:16:29 +0800

    打包工具 tools/setup.py

---
+++ iMan_Server/tests/2015-5-19/li1.py
@@ -0,0 +1,6 @@
+import mylist
+cast = ['Cleese', 'Palin', 'Jones', 'Idle', ['tmp', 'tmp2', ['in1', 'in2']], 'ok']
+mylist.print_list(cast)
+
+list2 = ['tmp', 'tmp2']
+# print(isinstance(list2, list))
---
+++ iMan_Server/tools/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name         = 'PIL',
+    version      = '1.1.8',
+    packages     = ['PIL'],
+    package_data = {'PIL':['*.pyd', '*.md']},
+    Summary      = 'cody.guo update',
+    author       = 'cody.guo',
+    author_email = 'hp131@hupu.net',
+    url          = 'http://pythonware.com/',
+    description  = 'cody.guo update PIL for python3.4',
+    Platform     = 'any',
+    )
